5ede753c07cd8015e357042435ee32a44171e466
Víctor Coelho <victor.coelho@codex.com.br>
2022-09-27 17:38:43 -0300

    [app][drivers] creating fake data for sensorInfo endpoint

--- src/app/run.py
+++ src/app/run.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import src.drivers.mocks.dash_web_mocker as web
 
-
 # Criando aplicação
 app = FastAPI()
 
@@ -14,19 +13,25 @@ def root() -> dict:
     """Rota raiz"""
     return {"message": "Hello World"}  # Retornando um dicionário
 
+
 @app.get("/users")
-def listando_date_user() -> BaseModel:
+def listando_date_user() -> list:
     """Listando todos dados"""
     return web.create_user(1000)
+
 
 @app.get("/times")
 def get_temp_history() -> list:
     """Retornando tempo para cada tipo de temperatura"""
-    return web.get_temp_history(27,30,num_teste=10)
-    
+    return web.get_temp_history(27, 30, num_teste=10)
+
+
 @app.get("/timeLimites")
 def get_limite():
     """Retornando tempo limite"""
-    return web.get_limite(20,40)
+    return web.get_limite(20, 40)
 
 
+@app.get("/sensorsInfo")
+def get_sensors_info():
+    return web.create_fake_sensors(10)
--- src/drivers/mocks/dash_web_mocker.py
+++ src/drivers/mocks/dash_web_mocker.py
@@ -12,11 +12,57 @@ class User(BaseModel):
     password: str
     access: str
 
-def create_user(num_user:int)-> list:
-    num_user = (num_user *3)
+
+class Metrics(BaseModel):
+    id: int
+    metric_name: str
+    metric: int
+
+
+class Sensor(BaseModel):
+    id: int
+    sensor_name: str
+    metrics: list
+    has_exceeded_limit: bool
+
+
+def create_fake_metrics(num_metrics: int) -> list:
+    metrics_list = []
+
+    for metric_id in range(1, num_metrics):
+        metrics_list.append(
+            Metrics(
+                id=metric_id,
+                metric_name="Temperatura {}".format(metric_id),
+                metric=get_temp_atual(0, 100)
+            )
+        )
+    return metrics_list
+
+
+def create_fake_sensors(num_sensors: int) -> list:
+    sensors_list = []
+
+    for sensor_id in range(1, num_sensors):
+        metrics = []
+        for metric in range(1, 5):
+            metrics.append(get_limite(20, 100))
+        sensors_list.append(
+            Sensor(
+                id=sensor_id,
+                sensor_name="Sensor{}".format(sensor_id),
+                metrics=create_fake_metrics(3),
+                has_exceeded_limit=get_has_exceeded_limit(sensor_id)
+            )
+        )
+    return sensors_list
+
+
+def create_user(num_user: int) -> list:
+    num_user = (num_user * 3)
     list_date_user = []
-    for num in range(1,999,3):
-        if(num%2):
+    for num in range(1, 999, 3):
+        if (num % 2):
             list_date_user.append(User(
                 id=num,
                 email=f"teste0{num}@gmail.com",
@@ -32,7 +78,8 @@ def create_user(num_user:int)-> list:
                 access="User"))
     return list_date_user
 
-def get_temp_history(init_temp:int,fim_temp:int,num_teste=20) -> list:
+
+def get_temp_history(init_temp: int, fim_temp: int, num_teste=20) -> list:
     """Retornando tempo para cada tipo de temperatura"""
     my_history = []
     for i in range(1, num_teste):
@@ -40,15 +87,22 @@ def get_temp_history(init_temp:int,fim_temp:int,num_teste=20) -> list:
             [
                 {
                     "time": f"2022-09-{i}T{j}:00:00",
-                    "temperature": random.uniform(init_temp,fim_temp),
+                    "temperature": random.uniform(init_temp, fim_temp),
                 }
                 for j in range(1, 24)
             ]
         )
     return my_history
 
-def get_limite(init_temp=20,fim_temp=40):
-    """Retornando tempo limite"""
-    temp_atual = random.randint(init_temp,fim_temp)
-    return {"litmit": temp_atual}
 
+def get_limite(init_temp=20, fim_temp=40):
+    """Retornando tempo limite"""
+    temp_atual = random.randint(init_temp, fim_temp)
+    return {"limit": temp_atual}
+
+def get_temp_atual(init_temp=20, fim_temp=40) -> int:
+    return random.randint(init_temp, fim_temp)
+
+
+def get_has_exceeded_limit(num: int) -> bool:
+    return True if num % 2 else False

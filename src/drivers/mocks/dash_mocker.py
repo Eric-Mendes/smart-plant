f02bb8a28504cfc5604e5353c9f66e7e1f9bd95e
kaiotakuma <kaiotakuma@gmail.com>
2022-10-04 16:05:52 -0300

    Add sensor_builder and build_temp_history

--- src/drivers/mocks/dash_mocker.py
+++ src/drivers/mocks/dash_mocker.py
@@ -1,4 +1,6 @@
 # disable pylint(no-name-in-module)
+import datetime
+import math
 import random
 from pydantic import BaseModel
 
@@ -24,7 +26,38 @@ class Sensor(BaseModel):
     sensor_name: str
     metrics: list
     has_exceeded_limit: bool
+class MySensor(BaseModel):
+    name:str
+    located_at:str
+    limit_value:float
 
+    # my_sensor = {
+    #     "name": input_json["name"],
+    #     "located_at": input_json["located_at"],
+    #     "limit_value": input_json["limit_value"]
+    # }
+
+NAME_LIST = ["Temperature", "Humidity", "Potency"]
+LOCATIONS = ["Room", "Boiler", "Cooler"]
+
+def sensor_builder(sensor_id:int) -> dict:
+    return {
+        "id": sensor_id,
+        "name": NAME_LIST[random.randint(0,len(NAME_LIST)-1)] + f"_{random.randint(0,999)}",
+        "located_at": LOCATIONS[random.randint(0,len(LOCATIONS)-1)] + f"_{random.randint(0,999)}",
+        "limit_value": random.randint(30, 50)
+    }
+
+def build_temp_history(sensor_id:int, n_days:int)->list:
+    base = datetime.datetime.today()
+    date_list = [str(base - datetime.timedelta(days=x, hours=y)) for x in range(n_days) for y in range(0,25)]
+
+    temp_history = [{
+        "timestamp": date,
+        "value": random.uniform(21,25) + 10*math.log10(sensor_id)
+    } for date in date_list]
+    
+    return temp_history
 
 def create_fake_metrics(num_metrics: int) -> list:
     metrics_list = []

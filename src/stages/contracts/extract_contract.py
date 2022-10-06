17776d6dfffda65c60417f0125fd928eb114377a
kaiotakuma <kaiotakuma@gmail.com>
2022-10-05 23:22:43 -0300

    Adicionando contract extract e transform

---
+++ src/stages/contracts/extract_contract.py
@@ -0,0 +1,12 @@
+from collections import namedtuple
+
+ExtractContract = namedtuple(
+    'ExtractContract',
+    '''
+        raw_information_content
+        extraction_date
+    '''
+
+)
+
+
---
+++ src/stages/contracts/transform_contract.py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+
+''' Como de inicio temos apenas um banco de dados vamos deichar apenas um elemento em nosso Transform contract.'''
+TransformContract = namedtuple(
+    'TransformContract',
+    '''
+        load_content
+    '''
+
+
+)

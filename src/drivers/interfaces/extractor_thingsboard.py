8dab02063df1336d5719498ab8b245dcd749dd63
kaiotakuma <kaiotakuma@gmail.com>
2022-10-05 23:23:43 -0300

    Adicionando interface extract

---
+++ src/drivers/interfaces/extractor_thingsboard.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class ExtractorThingsBoardInterface(ABC):
+    """Colocar docstring"""
+
+    @classmethod
+    def extract_thingsboard(cls)-> dict:
+        '''asd'''
+        pass
+

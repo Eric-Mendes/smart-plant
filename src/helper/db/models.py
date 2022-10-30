7e5cc513309dcc86cba6bf4bb4e83f1de5386853
kaiotakuma <kaiotakuma@gmail.com>
2022-10-30 17:22:21 -0300

    Add helper/db/

---
+++ src/helper/db/models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Info(BaseModel):
+    type: str
+    upper_limit: Optional[float] = False
+    bottom_limit: Optional[float] = False
+    
+class SensorPatch(BaseModel):
+    located_at: Optional[str] = False
+    info: Optional[List[Info]]
+    groups: Optional[list] = False
